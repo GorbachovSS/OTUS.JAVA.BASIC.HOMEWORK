cc7e0ca3b5c2bc4d870bf40560f25c2b395272f5
Sergey <st0pcheats@yandex.ru>
2023-07-31 17:31:10 +0300

    Исправил замечания к ДЗ урок №7

--- src/main/java/OTUS/JAVA/BASIC/HOMEWORK/lesson7/HomeWorkLesson7.java
+++ src/main/java/OTUS/JAVA/BASIC/HOMEWORK/lesson7/HomeWorkLesson7.java
@@ -6,11 +6,11 @@ public class HomeWorkLesson7 {
     public static void main(String[] args) {
         sumOfPositiveElements(new int[5][5]);
         System.out.println();
-        drawBox(5, 5);
+        drawBox(5);
         System.out.println();
-        diagonalElements(new int[10][10]);
+        diagonalElements(new int[5][5]);
         System.out.println();
-        findMax(new int[5][5]);
+        findMax(new int[6][7]);
         System.out.println();
         sumTwoLine(new int[5][5]);
     }
@@ -35,31 +35,29 @@ public class HomeWorkLesson7 {
         System.out.println("Сумма чисел массива > 0: " + sum);
     }
 
-    public static void drawBox(int size_X, int size_Y) {
-        char[][] array = new char[size_X][size_Y];
-        for (int i = 0; i < size_X; i++) {
-            for (int j = 0; j < size_Y; j++) {
-                array[i][j] = '*';
-            }
-        }
-        for (int i = 0; i < size_X; i++) {
-            for (int j = 0; j < size_Y; j++) {
-                System.out.print(array[i][j] + " ");
+    public static void drawBox(int size) {
+
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" * ");
             }
             System.out.println();
         }
     }
 
+
     static public void diagonalElements(int[][] arrays) {
         for (int i = 0; i < arrays.length; i++) {
             for (int j = 0; j < arrays[i].length; j++) {
-                if (j < 5) {
-                    System.out.print(5 + " ");
+                if (i < j) {
+                    arrays[i][j] = 1;
                 } else {
-                    System.out.print(0 + " ");
+                    arrays[i][j] = 0;
                 }
+                System.out.print(arrays[i][j] + " ");
             }
-            System.out.println();
+            System.out.print("\n");
         }
     }
 
@@ -84,26 +82,31 @@ public class HomeWorkLesson7 {
             }
         }
         System.out.println("Максимальное число массива = " + max);
-        System.out.println("Максимальное число массива = " + min);
+        System.out.println("Минимальное число массива = " + min);
         return max + min;
 
     }
 
-    public static int sumTwoLine(int[][] array) {
-        Random random = new Random();
-        int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = random.nextInt(10);
-                System.out.print(array[i][j] + " ");
+
+public static int sumTwoLine(int[][] array) {
+    Random random = new Random();
+    int count = 0;
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+            array[i][j] = random.nextInt(10);
+            System.out.print(array[i][j] + " ");
+            if (j == 2){
+                count += array[i][j];
             }
-            System.out.println();
+
         }
-        count = array[1][0] + array[1][1] + array[1][2] + array[1][3] + array[1][4];
-
-
-        System.out.println("Сумма 2 сторки = " + count);
-
-        return count;
+        System.out.println();
     }
+
+
+
+    System.out.println("Сумма 2 сторки = " + count);
+
+    return count;
+}
 }
