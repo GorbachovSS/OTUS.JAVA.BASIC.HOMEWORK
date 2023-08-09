744640f82aa9ab089b29a075837f4e1cafa5591d
Sergey <st0pcheats@yandex.ru>
2023-08-09 18:52:25 +0300

    Урок №12 Выполнено

---
+++ src/main/java/OTUS/JAVA/BASIC/HOMEWORK/lesson12/Cat.java
@@ -0,0 +1,50 @@
+package otus.java.basic.homework.lesson12;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+    public void eat(Plate plate) {
+        satiety = plate.reducingCountFood(appetite);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", appetite=" + appetite +
+                ", satiety=" + satiety +
+                '}';
+    }
+}
