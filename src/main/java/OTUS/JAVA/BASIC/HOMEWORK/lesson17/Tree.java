f229f5a327be7ab09fb7890270390899300bc8f5
Sergey <st0pcheats@yandex.ru>
2023-10-01 17:53:45 +0300

    Урок №17 Первый коммит

---
+++ src/main/java/OTUS/JAVA/BASIC/HOMEWORK/lesson17/Tree.java
@@ -0,0 +1,31 @@
+package otus.java.basic.homework.lesson17;
+
+
+public class Tree {
+    private Node rootNode; // корневой узел
+
+    public Tree() { // Пустое дерево
+        rootNode = null;
+    }
+
+    public Node findNodeByValue(int value) { // поиск узла по значению
+        Node currentNode = rootNode; // начинаем поиск с корневого узла
+        while (currentNode.getValue() != value) { // поиск покуда не будет найден элемент или не будут перебраны все
+            if (value < currentNode.getValue()) { // движение влево?
+                currentNode = currentNode.getLeftChild();
+            } else { //движение вправо
+                currentNode = currentNode.getRightChild();
+            }
+            if (currentNode == null) { // если потомка нет,
+                return null; // возвращаем null
+            }
+        }
+        return currentNode; // возвращаем найденный элемент
+    }
+
+    public void insertNode(int i) {
+    }
+
+    public void printTree() {
+    }
+}
